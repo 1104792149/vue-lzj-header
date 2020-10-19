b681b5b9d5eaf15f34d74e0b524555d1dd206036
lzj <1104792149@qq.com>
2020-10-19 15:41:54 +0800

    import vuePayKeyboard from '../dist/vue-lzj-header.js'

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuePayKeyboard from './lib/index.js'
-// import vuePayKeyboard from './lib/indexvue-lzj-header'
+import vuePayKeyboard from './lib/index.js'//开发时的路径
+import vuePayKeyboard from '../dist/vue-lzj-header.js'//打包时的路径
 Vue.use(vuePayKeyboard)
 new Vue({
   el: '#app',
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  // entry: './src/main.js', // 开发时项目入口
-  entry: './src/lib/index.js', // 打包发布时入口
+  entry: './src/main.js', // 开发时项目入口
+  // entry: './src/lib/index.js', // 打包发布时入口
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
     // filename: 'build.js', // 打包后输出的文件名
     filename: 'vue-lzj-header.js', // 我们可不想打包后叫build 多low啊 起一个与项目相对应的
-    library: 'PayKeyboard', // library指定的就是你使用require时的模块名，这里便是require("PayKeyboard")
+    library: 'lzjHeader', // library指定的就是你使用require时的模块名，这里便是require("lzjHeader")
     libraryTarget: 'umd', //libraryTarget会生成不同umd的代码，例如可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的。
     umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define。
   },
